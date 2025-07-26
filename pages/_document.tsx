b7b586978aa3532ceac8567fcbe4b65fb51c3eb8
Arkadiusz <a4p.email@gmail.com>
2025-07-26 11:45:53 +0200

    Add Google Ads global site tag (gtag.js) to _document.tsx for campaign tracking

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,6 +10,19 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Great+Vibes&display=swap"
             rel="stylesheet"
           />
+
+          {/* Global site tag (gtag.js) - Google Ads */}
+          <script async src="https://www.googletagmanager.com/gtag/js?id=AW-17395942813"></script>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', 'AW-17395942813');
+              `,
+            }}
+          />
         </Head>
         <body>
           <Main />
