74d4ad018fe2caa35097ed3f1e2138e19f5da464
Arkadiusz <a4p.email@gmail.com>
2025-10-16 22:08:02 +0200

    Add Google Ads global tag and conversion tracking

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,36 +1,32 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document';
+import { Html, Head, Main, NextScript } from 'next/document';
 
-class MyDocument extends Document {
-  render() {
-    return (
-      <Html lang="pl">
-        <Head>
-          {/* Czcionka ręcznego pisma */}
-          <link
-            href="https://fonts.googleapis.com/css2?family=Great+Vibes&display=swap"
-            rel="stylesheet"
-          />
-
-          {/* Global site tag (gtag.js) - Google Ads */}
-          <script async src="https://www.googletagmanager.com/gtag/js?id=AW-17395942813"></script>
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-                gtag('config', 'AW-17395942813');
-              `,
-            }}
-          />
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
+export default function Document() {
+  return (
+    <Html lang="pl">
+      <Head>
+        {/* Google Ads Tag */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=AW-17395428138"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'AW-17395428138');
+            `,
+          }}
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
 }
 
-export default MyDocument;
+
+
