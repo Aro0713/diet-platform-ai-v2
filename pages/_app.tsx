606b838a1e0fe96c5c75e16d367772858d7a3d18
Arkadiusz <a4p.email@gmail.com>
2026-01-13 11:53:55 +0100

    Add Vercel Analytics to track page views

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,17 @@
-import React from 'react';
-import '../styles/globals.css';
-import type { AppProps } from 'next/app';
-import { appWithTranslation } from 'next-i18next';
-import '@fontsource/dancing-script'; // font-handwriting
-import CookieBanner from '@/components/CookieBanner';
+import React from "react";
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { appWithTranslation } from "next-i18next";
+import "@fontsource/dancing-script"; // font-handwriting
+import CookieBanner from "@/components/CookieBanner";
+import { Analytics } from "@vercel/analytics/next";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <div className="min-h-screen bg-gray-50 text-gray-800 font-sans">
       <Component {...pageProps} />
       <CookieBanner />
+      <Analytics />
     </div>
   );
 }
